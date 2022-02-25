5c12483086f19d1c9f57c45126213868042ab4c4
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2022-02-24 20:03:34 -0500

    Now using IntelliJ's ComboBox.

--- src/main/java/com/oliveryasuna/idea/qemu/run/ui/QemuRunConfigurationEditor.java
+++ src/main/java/com/oliveryasuna/idea/qemu/run/ui/QemuRunConfigurationEditor.java
@@ -3,8 +3,10 @@ package com.oliveryasuna.idea.qemu.run.ui;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.ComboBox;
+import com.intellij.openapi.ui.ComponentValidator;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.ui.JBColor;
+import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.ui.ListCellRendererWithRightAlignedComponent;
 import com.jetbrains.cidr.cpp.cmake.model.CMakeTarget;
 import com.jetbrains.cidr.cpp.execution.CMakeBuildConfigurationHelper;
@@ -12,12 +14,12 @@ import com.oliveryasuna.idea.qemu.run.config.QemuRunConfiguration;
 import com.oliveryasuna.idea.qemu.run.config.QemuRunConfigurationOptions;
 import com.oliveryasuna.idea.qemu.util.ExecutableUtils;
 import com.oliveryasuna.idea.qemu.util.QemuExecutableFinder;
+import org.apache.commons.lang3.StringUtils;
 import org.jdesktop.swingx.combobox.ListComboBoxModel;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
 import java.io.File;
 
 // I hate Swing...
@@ -37,8 +39,6 @@ public class QemuRunConfigurationEditor extends SettingsEditor<QemuRunConfigurat
 
   private final Project project;
 
-  private Border qemuExecutableFieldDefaultBorder;
-
   private Border qemuExecutableFieldErrorBorder;
 
   // UI components
@@ -46,12 +46,11 @@ public class QemuRunConfigurationEditor extends SettingsEditor<QemuRunConfigurat
 
   private JPanel rootPanel;
 
-  // TODO: Allow custom items.
-  private JComboBox<String> qemuExecutableField;
+  private ComboBox<String> qemuExecutableField;
 
   private JRadioButton cmakeTargetRadio;
 
-  private JComboBox<CMakeTarget> cmakeTargetField;
+  private ComboBox<CMakeTarget> cmakeTargetField;
 
   private JRadioButton cdromFileRadio;
 
@@ -93,12 +92,9 @@ public class QemuRunConfigurationEditor extends SettingsEditor<QemuRunConfigurat
 
   @Override
   protected final JComponent createEditor() {
-    qemuExecutableFieldDefaultBorder = qemuExecutableField.getBorder();
-
     qemuExecutableField.setModel(new DefaultComboBoxModel<>(QemuExecutableFinder.getQemuExecutables().stream()
         .map(File::getAbsolutePath)
         .toArray(String[]::new)));
-    qemuExecutableField.addItemListener(this::qemuExecutableFieldChanged);
 
     cmakeTargetRadio.addActionListener(this::diskImageSourceRadioChanged);
     cdromFileRadio.addActionListener(this::diskImageSourceRadioChanged);
@@ -106,24 +102,70 @@ public class QemuRunConfigurationEditor extends SettingsEditor<QemuRunConfigurat
     cmakeTargetField.setModel(new ListComboBoxModel<>(new CMakeBuildConfigurationHelper(project).getTargets()));
     cmakeTargetField.setRenderer(new CMakeTargetCellRenderer());
 
+    addQemuExecutableFieldValidator();
+    addCMakeTargetFieldValidator();
+    addCdromFileFieldValidator();
+
     return rootPanel;
+  }
+
+  // Validation methods
+  //--------------------------------------------------
+
+  private void addQemuExecutableFieldValidator() {
+    new ComponentValidator(project)
+        .withValidator(() -> {
+          final String qemuExecutableCandidate = qemuExecutableField.getItem();
+
+          if(StringUtils.isBlank(qemuExecutableCandidate)) return new ValidationInfo("Required field.", qemuExecutableField);
+          if(!ExecutableUtils.canExecute(qemuExecutableCandidate)) return new ValidationInfo("Not found.", qemuExecutableField);
+
+          return null;
+        })
+        .installOn(qemuExecutableField);
+
+    qemuExecutableField.addActionListener(event -> ComponentValidator.getInstance(qemuExecutableField)
+        .ifPresent(ComponentValidator::revalidate));
+  }
+
+  private void addCMakeTargetFieldValidator() {
+    new ComponentValidator(project)
+        .withValidator(() -> {
+          final CMakeTarget cmakeTargetCandidate = cmakeTargetField.getItem();
+
+          if(cmakeTargetField.isEnabled() && cmakeTargetCandidate == null) {
+            return new ValidationInfo("Required field.", cmakeTargetField);
+          }
+
+          return null;
+        })
+        .installOn(cmakeTargetField);
+
+    cmakeTargetField.addActionListener(event -> ComponentValidator.getInstance(cmakeTargetField)
+        .ifPresent(ComponentValidator::revalidate));
+  }
+
+  private void addCdromFileFieldValidator() {
+    // TODO: Doesn't work.
+
+    new ComponentValidator(project)
+        .withValidator(() -> {
+          final String cdromFileCandidate = cdromFileField.getText();
+
+          if(cdromFileField.isEnabled() && StringUtils.isBlank(cdromFileCandidate)) {
+            return new ValidationInfo("Required field.", cdromFileField);
+          }
+
+          return null;
+        })
+        .installOn(cdromFileField);
+
+    cdromFileField.addActionListener(event -> ComponentValidator.getInstance(cdromFileField)
+        .ifPresent(ComponentValidator::revalidate));
   }
 
   // Listener callbacks
   //--------------------------------------------------
-
-  private void qemuExecutableFieldChanged(final ItemEvent event) {
-    final String qemuExecutableCandidate = (String)event.getItem();
-
-    new Thread(() -> {
-      // TODO: Better way of doing border.
-      if(ExecutableUtils.canExecute(qemuExecutableCandidate)) {
-        qemuExecutableField.setBorder(qemuExecutableFieldDefaultBorder);
-      } else {
-        qemuExecutableField.setBorder(BorderFactory.createLineBorder(JBColor.RED));
-      }
-    }).start();
-  }
 
   private void diskImageSourceRadioChanged(final ActionEvent event) {
     final JRadioButton source = (JRadioButton)event.getSource();
